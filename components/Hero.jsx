8fa2e4e3256c8822eeb612df3370f4d9a63213ca
gugisek <gugisek@gmail.com>
2023-01-13 14:01:15 +0100

    Add tailwind and test component structure

---
+++ components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <section className="flex flex-col items-center justify-center w-full h-screen bg-neutral-900">
+        <h1 className="text-5xl font-bold text-neutral-100">Praktyczny Informatyk</h1>
+        <p className="mt-2 text-lg text-neutral-100">Blog o informatyce</p>
+        </section>
+    );
+    };
